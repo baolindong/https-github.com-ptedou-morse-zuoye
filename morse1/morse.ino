6d6694ac62f54c41324494d997394fd7f0193791
baolindong <52458407+baolindong@users.noreply.github.com>
2019-07-04 23:06:50 +0800

    Add files via upload

---
+++ morse1/morse.ino
@@ -0,0 +1,78 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+char MORSE[][4] = {
+  {'.', '-', '*', '*'}, //a
+  {'-', '.', '.', '.'}, //b
+  {'-', '.', '-', '.'}, //c
+  {'-', '.', '.', '*'}, //d
+  {'.', '*', '*', '*'}, //e
+  {'.', '.', '-', '.'}, //f
+  {'-', '-', '.', '*'}, //g
+  {'.', '.', '.', '.'}, //h
+  {'.', '.', '*', '*'}, //i
+  {'.', '-', '-', '-'}, //j
+  {'-', '.', '-', '*'}, //k
+  {'.', '-', '.', '.'}, //l
+  {'-', '-', '*', '*'}, //m
+  {'-', '.', '*', '*'}, //n
+  {'-', '-', '-', '*'}, //o
+  {'.', '-', '-', '.'}, //p
+  {'-', '-', '.', '-'}, //q
+  {'.', '-', '.', '*'}, //r
+  {'.', '.', '.', '*'}, //s
+  {'-', '*', '*', '*'}, //t
+  {'.', '.', '-', '*'}, //u
+  {'.', '.', '.', '-'}, //v
+  {'.', '-', '-', '*'}, //w
+  {'-', '.', '.', '-'}, //x
+  {'-', '.', '-', '-'}, //y
+  {'-', '-', '.', '.'}  //z
+};
+int ma = 13;
+void dot() {   digitalWrite( ma, HIGH);   delay(1000);   digitalWrite( ma, LOW);   delay(250); }
+void setup() {
+  Serial.begin(9600);
+   pinMode( ma, OUTPUT); 
+}
+
+void loop() {
+  String str = ""; 
+  String morse = ""; 
+  int i, t , temp = 0;
+  int n = 0;
+  while (Serial.available() > 0) {
+    temp = 1;  
+    str += char(Serial.read());
+    delay(2);
+    n++;
+  }
+
+  if (temp) {
+    for (i = 0; i < n; i++)
+    {
+      for (t = 0; t < 4; t++)
+      {
+        if (str[i] >= 97 && str[i] <= 122)
+        {
+          morse += char(MORSE[int(str[i] - 97)][t]);
+        }
+      }
+      morse += ' ';
+    }
+    Serial.println(morse);
+    for(i=0;i<=4*n;i++)
+    {if(morse[i]=='.')
+      dot();
+     if(morse[i]=='-')
+      dash();
+    }
+  }
+  delay(2);
+}
